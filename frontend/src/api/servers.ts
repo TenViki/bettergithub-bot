9aa38a010029e552ea8e28fabfcaa418cef8c712
TenViki <viktorkomarek28@gmail.com>
2022-07-13 19:03:07 +0200

    :sparkles: Adds create webhook api request

--- frontend/src/api/servers.ts
+++ frontend/src/api/servers.ts
@@ -12,11 +12,41 @@ export const getGuilds = (token: string) => {
 
 export const getChannels = (token: string, guildId: string) => {
   return server.get<{
-    channels: (GuildBasedChannel & { canSendMessages: boolean })[];
+    channels: (GuildBasedChannel & {
+      canSendMessages: boolean;
+      webhook?: {
+        guild: string;
+        channel: string;
+        _id: string;
+      };
+    })[];
     guild: UserGuilds;
   }>(`/discord/guilds/${guildId}`, {
     headers: {
       Authorization: token,
     },
   });
+};
+
+export const createWebhook = (
+  token: string,
+  guildId: string,
+  channelId: string
+) => {
+  return server.post<{
+    guild: string;
+    channel: string;
+    _id: string;
+  }>(
+    `/discord/guilds/${guildId}/${channelId}`,
+    {
+      guild: guildId,
+      channel: channelId,
+    },
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
 };
