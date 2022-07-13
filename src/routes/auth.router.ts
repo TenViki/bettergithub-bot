8d6a73e5ee13139ce97bbc2a88fb16ae7216e041
TenViki <viktorkomarek28@gmail.com>
2022-07-13 10:48:58 +0200

    :sparkles: Adds server url config

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -6,4 +6,15 @@ authRouter.get("/", (req, res) => {
   res.send("Auth");
 });
 
+authRouter.post("/", (req, res) => {
+  if (!req.body.code) {
+    res.status(400).send({ error: "Missing code" });
+  }
+
+  res.send({
+    code: req.body.code,
+    message: "Code received",
+  });
+});
+
 export default authRouter;
